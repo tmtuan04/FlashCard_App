073fd9f12c1e3baf3b9c94b90b25abdd140d7e0c
tmtuan04 <tuan04tm@gmail.com>
2024-06-01 21:39:41 +0700

    end 1/5

--- src/main/java/example/demo/HelloApplication.java
+++ src/main/java/example/demo/HelloApplication.java
@@ -2,6 +2,7 @@ package example.demo;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,9 +11,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        Scene scene = new Scene(root);
+//      Hay lỗi ở đoạn source
+        String css = this.getClass().getResource("/css/main.css").toExternalForm();
+        scene.getStylesheets().add(css);
+
+        stage.setTitle("EngHUST");
         stage.setScene(scene);
         stage.show();
     }
