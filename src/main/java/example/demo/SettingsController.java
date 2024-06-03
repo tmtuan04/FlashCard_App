1585d3449f5e3bf2ac0b6e7b286798f040da4ca7
tmtuan04 <tuan04tm@gmail.com>
2024-06-03 11:56:34 +0700

    tuan commit 3/6

---
+++ src/main/java/example/demo/SettingsController.java
@@ -0,0 +1,14 @@
+package example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SettingsController {
+
+}
