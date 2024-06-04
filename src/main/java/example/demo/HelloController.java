c003f0cb5c3b51f7c57be878265b2a98ac3bf51e
tmtuan04 <tuan04tm@gmail.com>
2024-06-04 20:29:10 +0700

    tuan commit 4/6

--- src/main/java/example/demo/HelloController.java
+++ src/main/java/example/demo/HelloController.java
@@ -14,10 +14,14 @@ public class HelloController {
     private Scene scene;
     private Parent root;
 
+    // Cái này vẫn chỉ là chuyển scene, vẫn chưa phải là 2 window khác nhau
+
+
     public void switchtoSettings(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Settings.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
+//        stage.setTitle("Settings");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/example/demo/SettingsController.java
+++ src/main/java/example/demo/SettingsController.java
@@ -9,6 +9,19 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+// Cái này vẫn chỉ là chuyển scene, vẫn chưa phải là 2 window khác nhau
+
 public class SettingsController {
 
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchtoMain(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("MainView.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
